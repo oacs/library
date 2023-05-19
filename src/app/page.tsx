6af2d9a5843ba497392135e5aa32650042cbb490
Oscar Castillejo <oacs1221@gmail.com>
2023-05-19 14:26:56 -0400

    patch: add key to map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ export default async function Home() {
   const test = await db.select().from(books).execute();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {test && test.map((book) => <li>{JSON.stringify(book)}</li>)}
+      {test &&
+        test.map((book) => <li key={book.id}>{JSON.stringify(book)}</li>)}
     </main>
   );
 }
